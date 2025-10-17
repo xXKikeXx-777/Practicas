4fa6be7ce446980cbbf8e991902e47514ffbbc02
Enrique <tuemail@dominio.com>
2025-10-16 22:27:06 -0600

    Agrego la función para promedio

--- Practica2/promediosDinamicos.c
+++ Practica2/promediosDinamicos.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h> // Necesario para malloc, realloc y free
+#include <stdlib.h>
+
+// Declaración de funciones
+int ingresarCalificaciones(float **calificaciones, int total);
+void liberarMemoria(float *calificaciones);
 
 void main(){
     printf("=== Calculadora de Promedio Dinámica ===\n\n");
     int opcion = 0;
-    float *calificaciones = NULL; // Puntero para almacenar calificaciones dinámicamente
-    int total_calificaciones = 0; // Contador del total de calificaciones ingresadas
+    float *calificaciones = NULL;
+    int total_calificaciones = 0;
 
     while(opcion != 2){
         printf("1) Ingresar calificaciones\n");
@@ -15,17 +19,20 @@ void main(){
 
         switch(opcion){
         case 1:
+            total_calificaciones = ingresarCalificaciones(&calificaciones, total_calificaciones);
+            break;
 
         case 2:
             //se libera la memoria reservada
+            liberarMemoria(calificaciones);
 
             //mensaje de despedida
-            printf("\nMemoria liberada!!! Hasta luego.\n");
+            // ya está dentro de liberarMemoria
             break;
+
         default:
             //opcion del usuario invalida
             printf("\n=== Opcion invalida === \nPor favor seleccione una opcion del menu:\n");
         }
     }
-
 }
